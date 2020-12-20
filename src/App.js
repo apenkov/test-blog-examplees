13496fd3da7292476dcb23a17c9a9783f95d5889
apenkov <organaizer1212@gmail.com>
2020-12-20 21:50:35 +0300

    create post posts components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+const posts =
+  [
+    {
+      title: 'first post',
+      littleText: 'preview text',
+      text: 'big text'
+    }, {
+    title: 'second post',
+    littleText: 'preview text',
+    text: 'big text'
+}
+]
+
+
+function Post(props) {
+  const post = props.post;
+  return (
+      <div className="post">
+        <p className="title">{post.title}</p>
+        <p className="preview" >{post.littleText}</p>
+        <p className={`body`} >{post.text}</p>
+      </div>
+  )
+}
+function Posts(props) {
+  const posts = props.posts;
+  const postsTemplate = posts.map((post, index)=>{
+    return (
+      <div key={index}>
+        <Post post ={post}/>
+      </div>
+    )
+  })
+  return(
+    <div>
+      {postsTemplate}
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Posts posts={posts}/>
     </div>
   );
 }
